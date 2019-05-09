649f9df8263d4119d6ade903c5e1b202ec196bf1
Bryce Eklund <bryce.eklund@gmail.com>
2019-05-08 17:31:01 -0700

    added datastore and began structuring code

---
+++ datastore.js
@@ -0,0 +1,52 @@
+const STORE = [
+    {
+        question: 'When was Ableton Live first released?',
+        answers: ['1998', '1993', '2004', '2001'],
+        correct: '2001'
+    },     
+    {
+        question: "What is the name of Ableton Live's parametric equalizer?",
+        answers: ['ParaEQ', 'EQ Three', 'EQ Eight', 'Equalizer'],
+        correct: 'EQ Eight'
+    },
+    {
+        question: 'In music production, changing the value of a parameter over time is called...?',
+        answers: ['Modulation', 'Automation', 'Parameterization', 'Novation'],
+        correct: 'Automation'
+    },
+    {
+        question: 'Sidechain compression can be achieved on which of the below Ableton Live devices?',
+        answers: ['Compressor', 'Glue Compressor', 'Multiband Compressor', 'All of the Above'],
+        correct: 'All of the Above'
+    },
+    {
+        question: 'Ableton Live was initially created as an instrument. What is the name of the view that this refers to?',
+        answers: ['Session View', 'Arrangement View', 'Player View', 'Timeline View'], 
+        correct: 'Session View'
+    },
+    {
+        question: 'What does MIDI stand for?',
+        answers: ['-Music Indication Delimiter Interface', 'Multi-Instrumental Delivery Instruction', 'Musical Instrument Digitial Interface', 'Markup Inductor Display Infrastructur'],
+        correct: 'Musical Instrument Digitial Interface'
+    },
+    {
+        question: 'Ableton has released a hardware device to augment Live. What is it called?',
+        answers: ['-Push', 'Hotpads', 'Sing', 'Launchpad'],
+        correct: 'Push'
+    },
+    {
+        question: 'What programming language is Ableton Live written in?',
+        answers: ['Python', 'Java', 'Perl', 'C++'],
+        correct: 'C++'
+    },
+    {
+        question: 'What is the name of the visual coding language that Ableton Live devices are written in?',
+        answers: ['Cycling74', 'Max', 'Moop', 'MLI'],
+        correct: 'Max'
+    },
+    {
+        question: 'What is the name of the new wavetable synth included in Ableton Live 10?',
+        answers: ['Wave', 'FM', 'Wavetable', 'Inductor'],
+        correct: 'Wavetable'
+    },
+];
---
+++ script.js
@@ -0,0 +1,19 @@
+function startQuiz() {
+    //starts the quiz when the "begin" button is clicked
+}
+
+function renderQuestion() {
+    //renders the current question + set of answers
+}
+
+function questionCounter() {
+    //returns current question and current score
+}
+
+function displayFinalScore() {
+    //returns score at the end of the quiz
+}
+
+function restartQuiz() {
+    //restarts quiz when restart button is clicked
+}
