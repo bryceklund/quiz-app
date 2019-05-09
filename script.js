d6983f8eead7b6efb572e03266014262996c6d1c
Bryce Eklund <bryce.eklund@gmail.com>
2019-05-08 22:44:35 -0700

    quiz begins properly; answers are handled semiproperly; counter increments

--- script.js
+++ script.js
@@ -1,13 +1,61 @@
-function startQuiz() {
-    //starts the quiz when the "begin" button is clicked
+let progress = 0;
+let score = 0;
+
+function progressCounter() {
+    console.log('`progressCounter` is running...');
+    //increments progress counter when called
+    progress += 1;
+}
+
+function handleCorrectAnswer(answer) {
+    //disabled other buttons, indicate correct answer in green, increment `score`
+    console.log('`handleCorrectAnswer` is running...');
+    $(answer).addClass('answer-correct');
+    $(answer).removeClass('answer');
+    $('.answer').prop('disabled', true);
+    score += 1;
+}
+
+function handleIncorrectAnswer(answer) {
+    console.log('`handleIncorrectAnswer` is running...');
+    $(answer).addClass('answer-incorrect');
+    $(answer).removeClass('answer');
+    $('.answer').prop('disabled', true);
+    $(STORE[progress]["correct"]) //need to highlight the correct answer. maybe need to implement an id systems????
+}
+
+function handleAnswerClicks() {
+    console.log('`handleAnswerClicks` is running...');
+    //calls incorrect/correct answer functions
+    $('.answer').click(function(event) {
+        event.preventDefault();
+        console.log($(event.currentTarget).html());
+        if ($(event.currentTarget).html() == STORE[progress]["correct"]) {
+            handleCorrectAnswer($(event.currentTarget));
+        } else {
+            handleIncorrectAnswer($(event.currentTarget));
+        }
+    });
 }
 
 function renderQuestion() {
+    console.log('`renderQuestion` is running...');
     //renders the current question + set of answers
+    $('main').html(`<p class="question">${STORE[progress]["question"]}</p>`);
 }
 
-function questionCounter() {
-    //returns current question and current score
+function renderAnswers() {
+    console.log('`renderAnswers` is running...');
+    $('main').append(`
+    <form class="answers">
+        <ul class="answers-list">
+            <button class="answer">${STORE[progress]["answers"][0]}</button>
+            <button class="answer">${STORE[progress]["answers"][1]}</button>
+            <button class="answer">${STORE[progress]["answers"][2]}</button>
+            <button class="answer">${STORE[progress]["answers"][3]}</button>
+        </ul>
+    </form>`);
+    handleAnswerClicks();
 }
 
 function displayFinalScore() {
@@ -18,10 +66,59 @@ function restartQuiz() {
     //restarts quiz when restart button is clicked
 }
 
-function headerToQuizMode() {
-    //moves the ableton logo over and shows the score and question trackers
+function displayScoreAndProgress() {
+    console.log('`displayScoreAndProgress` is running...');
+    //displays and updates the player's score
+    $('.score').text(`Score: ${score}`);
+    $('.progress').text(`${progress + 1}/10`);
 }
 
-function headerToIntroMode() {
-    //hides the score and question trackers; moves ableton logo to center
+
+function startQuiz() {
+    console.log('`startQuiz` is running...');
+    //starts the first question of the quiz
+    $('.intro-text').addClass('hidden');
+    $('.score').removeClass('hidden');
+    $('.progress').removeClass('hidden');
+    renderQuestion();
+    renderAnswers();
+    displayScoreAndProgress();
 }
+
+function nextQuestion() {
+    progressCounter();
+    renderQuestion();
+    renderAnswers();
+}
+
+function clearAnswers() {
+    console.log('`clearAnswers` is running...');
+    //clears the current question and answers from the screen
+    $('.answers-list').empty();
+    $('.question').remove();
+}
+
+function mainToIntroMode () {
+    console.log('`mainToIntroMode` is running...');
+    //resets the quiz to its starting position
+    clearAnswers();
+    $('.intro-text').removeClass('hidden');
+    $('.score').addClass('hidden');
+    $('.progress').addClass('hidden');
+}
+
+function handleBeginClicks() {
+    console.log('`handleBeginClicks` is running...');
+    //starts the quiz
+    $('.begin').click(function() {
+        console.log('starting quiz...');
+        startQuiz();
+    });
+}
+
+function initializeQuiz () {
+    mainToIntroMode();
+    handleBeginClicks();
+}
+
+$(initializeQuiz);
